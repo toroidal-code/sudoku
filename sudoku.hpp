94e4352858a7eabbe75e6012571ffbd2af19ded6
Katherine Whitlock <toroidalcode@gmail.com>
2013-04-23 10:34:15 -0400

    Second C++ commit

--- sudoku.hpp
+++ sudoku.hpp
@@ -8,16 +8,25 @@
 class SudokuConfig
 {
 public:
-	int** board;
+	int board[DIM][DIM];
 	SudokuConfig(int** _initial)
 	{
-		board = _initial;
+		int** board_as_ptr = (int**)board;
+		board_as_ptr = _initial;
+	}
+	SudokuConfig(SudokuConfig &config)
+	{
+		for (int i = 0; i < DIM; i++){
+			for (int j = 0; j < DIM; j++){
+				board[i][j] = config.board[i][j];
+			}
+		}
 	}
 	friend std::ostream& operator<< (std::ostream &out, SudokuConfig &config);
 	void values(int direction, int index, int* *array);
 	bool isValid();
 	bool isGoal();
-	void getSuccessors(SudokuConfig* array);
+	void getSuccessors(SudokuConfig* *array);
 	SudokuConfig solve(bool debug);
 
 };
