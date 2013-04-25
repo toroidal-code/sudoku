85448d7032199c49b57a48555dc668b09a70b803
Katherine Whitlock <toroidalcode@gmail.com>
2013-04-25 02:09:12 -0400

    New algorithm

--- python3/sudoku2.py
+++ python3/sudoku2.py
@@ -1,28 +1,39 @@
 #! /usr/bin/python3
-from copy import deepcopy
 from sys import argv
-import collections
+from copy import deepcopy
 import time
 import os
 
-class SudokuConfig:
-    """
-    A class that represents a sudoku configuration.
-        board - square grid of values 
-            (list of list of int's, size DIM*DIM)
-    """
-    __slots__ = ('DIM', 'board')
-    
-    # The empty cell value
-    EMPTY = 0   # can be referenced anywhere as: SudokuConfig.EMPTY
-    
-    def __init__(self,initial):
+DIM = 9
+
+
+class Cell:
+    __slots__=('value', 'possible')
+
+    def __init__(self, initial):
         """
-        Constructor.
+        Constructor
         """
-        self.DIM = 9
-        self.board = initial
-                    
+        self.value = initial
+        self.possible = [] # [0,0,0,0,0,0,0,0,0]
+
+    def __str__(self):
+        """
+        Return a string representation of the cell
+        """
+        return str(self.value)
+
+
+class Sudoku:
+    __slots__ = ('board')
+
+    EMPTY = 0
+
+    def __init__(self, initial):
+        self.board = [] * DIM
+        for row in initial:
+            self.board.append([Cell(value) for value in row])
+
     def __str__(self):
         """
         Return a string representation of the config.
@@ -30,91 +41,90 @@ class SudokuConfig:
         
         # top row
         result = '  '
-        result = '\n ' + '-' * (self.DIM*2+5) + '\n'
+        result = '\n ' + '-' * (DIM*2+5) + '\n'
             
         # board rows
-        for row in range(self.DIM):
+        for row in range(DIM):
             if row is 3 or row is 6:
-                result += '|' + '-' * (self.DIM*2+5) + '|' + '\n'
+                result += '|' + '-' * (DIM*2+5) + '|' + '\n'
                 # result += '|-------+-------+-------|\n'
             result += '| '
-            for col in range(self.DIM):
+            for col in range(DIM):
                 if col is 3 or col is 6:
                     result += '| '
-                if self.board[row][col] == SudokuConfig.EMPTY:
+                if self.board[row][col].value == 0:
                     result += '.'
                 else:
-                    result += str(str(self.board[row][col]))
-                if col != self.DIM-1:
+                    result += str(self.board[row][col])
+                if col != DIM-1:
                     result += ' '
             result += ' |' + '\n'
             
         # bottom row
-        result += ' ' + '-' * (self.DIM*2+5) + '\n'
+        result += ' ' + '-' * (DIM*2+5) + '\n'
         result += '  '
         result += '\n'
-                  
         return result
-    
+        
     def values(self, direction, index):
-        if direction == "r":
+        if direction == 0:
             temp = []
             for item in self.board[index]:
-                temp.append(item)
+                temp.append(item.value)
             return temp
-        elif direction == "c":
+        elif direction == 1:
             temp = []
             for row in self.board:
-                temp.append(row[index])
+                temp.append(row[index].value)
             return temp
-        elif direction == "s":
+        elif direction == 2:
             temp = []
             if index == 1:
                 for r in range(3):
                     for c in range(3):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 2:
                 for r in range(3):
                     for c in range(3,6):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 3:
                 for r in range(3):
                     for c in range(6,9):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 4:
                 for r in range(3,6):
                     for c in range(3):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 5:
                 for r in range(3,6):    
                     for c in range(3,6):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 6:
                 for r in range(3,6):    
                     for c in range(6,9):
-                        temp.append(self.board[r][c])       
+                        temp.append(self.board[r][c].value)       
             elif index == 7:
                 for r in range(6,9):
                     for c in range(3):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 8:
                 for r in range(6,9):    
                     for c in range(3,6):
-                        temp.append(self.board[r][c])
+                        temp.append(self.board[r][c].value)
             elif index == 9:
                 for r in range(6,9):    
                     for c in range(6,9):
-                        temp.append(self.board[r][c])         
+                        temp.append(self.board[r][c].value)         
             return temp
 
     def isValid(self):
-        for idx in range(self.DIM):
-            for direc in ['r','c','s']:
+        for idx in range(DIM):
+            for direc in range(3):
                 vals = self.values(direc, idx)
-                for num in range(1, self.DIM+1):
+                for num in range(1, DIM+1):
                     if vals.count(num) > 1:
                         return False
-        return True        
+        return True            
 
     def isGoal(self):
         """
@@ -122,28 +132,63 @@ class SudokuConfig:
             config: The config (SkyscraperConfig)
         Returns: True if config is a goal, False otherwise
         """
-        for index in range(self.DIM):
-            if not self.values('r',index).count(0) is 0:
+        for index in range(DIM):
+            if not self.values(0, index).count(0) is 0:
                 return False
         if not self.isValid():
             return False
         return True
 
+    def printPoss(self):
+        temp = []
+        for row in self.board:
+            print([cell.possible for cell in row])
+
+    def genPoss(self):
+        for row in range(DIM):
+            for col in range(DIM):
+                st = set(self.values(0, row)) | \
+                     set(self.values(1, col)) | \
+                     set(self.values(2, getQuadrant(row, col)))
+                if self.board[row][col].value is 0:
+                    self.board[row][col].possible = list(set(range(1,10)) - st)
+                else:
+                    self.board[row][col].possible = []
+
+    def process(self):
+        self.genPoss()
+        while(self.getMinPoss() is 1):
+            for row in range(DIM):
+                for col in range(DIM):
+                    poss = self.board[row][col].possible
+                    if len(poss) is 1:
+                        self.board[row][col].value = poss[0]
+            self.genPoss()
+
+
+    def getMinPoss(self):
+        l = DIM
+        for row in range(DIM):
+            for col in range(DIM):
+                poss = self.board[row][col].possible
+                if len(poss) < l and \
+                   len(poss) is not 0:
+                    l = len(poss)
+        return l
+
     def getSuccessors(self):
         suc = []
-        for row in range(self.DIM):
-            usedRowVals = self.values('r',row)
-            for col in range(self.DIM):
-                usedColVals = self.values('c',col)
-                usedSubVals = self.values('s',getQuadrant(row,col))
-                if self.board[row][col] == 0:
-                    for i in range(1,self.DIM+1):
-                        if usedRowVals.count(i) is 0 and usedColVals.count(i) is 0 and usedSubVals.count(i) is 0: #if we already used that value, don't reuse it
-                            if i == self.board[row][col]:
-                                pass
-                            newConfig = deepcopy(self)
-                            newConfig.board[row][col] = i
-                            suc.append(newConfig)
+        for row in range(DIM):
+            usedRowVals = self.values(0,row)
+            for col in range(DIM):
+                usedColVals = self.values(1,col)
+                usedSubVals = self.values(2,getQuadrant(row,col))
+                if self.board[row][col].value is 0 and len(self.board[row][col].possible) is self.getMinPoss():
+                    for i in self.board[row][col].possible:
+                        newConfig = deepcopy(self)
+                        newConfig.board[row][col].value = i
+                        newConfig.process()
+                        suc.append(newConfig)
                     return suc
 
         return []
@@ -155,7 +200,6 @@ class SudokuConfig:
             debug: print debug output? (Bool)
         Returns:  A config (SkyscraperConfig), if valid, None otherwise
         """
-
         if self.isGoal():
             return self
         else:
@@ -166,31 +210,6 @@ class SudokuConfig:
                     solution = successor.solve(debug)
                     if solution is not None:
                         return solution
-
-
-def readBoard(filename):
-    """
-    Read the board file.  It is organized as follows:
-        row 1 values    # 0 for empty, (1-DIM) otherwise
-        row 2 values    # 0 for empty, (1-DIM) otherwise
-        ...
-    
-        filename: The file name (string)
-    Returns: A config (SkyscraperConfig) containing the board info from file
-    """
-    
-    f = open(filename)
-    DIM = 9
-    board = []
-    for _ in range(DIM):
-        line = [int(val) for val in f.readline().split()]
-        board.append(line)
-    f.close()
-    return SudokuConfig(board)
-
-
-def remove_values_from_list(the_list, val):
-    return [value for value in the_list if value != val]
 
 
 def getQuadrant(row,col):
@@ -215,6 +234,25 @@ def getQuadrant(row,col):
             return 8
         if col >= 6:
             return 9
+
+def readBoard(filename):
+    """
+    Read the board file.  It is organized as follows:
+        row 1 values    # 0 for empty, (1-DIM) otherwise
+        row 2 values    # 0 for empty, (1-DIM) otherwise
+        ...
+    
+        filename: The file name (string)
+    Returns: A config (SkyscraperConfig) containing the board info from file
+    """
+    
+    f = open(filename)
+    board = []
+    for _ in range(DIM):
+        line = [int(val) for val in f.readline().split()]
+        board.append(line)
+    f.close()
+    return Sudoku(board)
 
 
 def main():
@@ -250,7 +288,9 @@ def main():
 
     initConfig = readBoard(filename)
     print('Initial Config:\n' + str(initConfig))
-    
+    initConfig.process()
+    print('Processed:\n' + str(initConfig))
+
     # solve the puzzle
     solution = initConfig.solve(debug)
     
@@ -264,6 +304,7 @@ def main():
         print('Solution:\n' + str(solution))
     else:
         print('No solution.')
-    
-if __name__ == '__main__':
-    main()
+
+
+main()
+
